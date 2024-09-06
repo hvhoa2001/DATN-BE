1f42fee842474ad9de42e74ad758ce9b7d4bc03c
hvhoa2001 <hvhoa2001@gmail.com>
2024-09-06 11:34:39 +0700

    create cors

--- index.ts
+++ index.ts
@@ -4,10 +4,13 @@ import express, { Request, Response } from "express";
 import { authRouter } from "./src/routes/user-services/authRouter";
 import { userRouter } from "./src/routes/user-services/userRouter";
 import connectMongoDB from "./connect";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT;
 
+app.options("*", cors());
+app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb" }));
 app.use("/auth", authRouter);
